76833b7038f8274310697eaef1b52862d93d8c00
LalithadityaAnchula <lalithaditya903@gmail.com>
2022-07-05 15:38:24 +0530

    remove unwanted comments

--- src/context/users/AdminAction.js
+++ src/context/users/AdminAction.js
@@ -6,20 +6,6 @@ const axiosInstance = axios.create({
   withCredentials: true,
 });
 
-// // Add a response interceptor
-// axiosInstance.interceptors.response.use(
-//   function (response) {
-//     // Any status code that lie within the range of 2xx cause this function to trigger
-//     // Do something with response data
-//     return response;
-//   },
-//   function (error) {
-//     // Any status codes that falls outside the range of 2xx cause this function to trigger
-//     // Do something with response error
-//     return error.response;
-//   }
-// );
-
 //Creating a city
 export const createCity = async (city) => {
   try {
--- src/context/users/UserAction.js
+++ src/context/users/UserAction.js
@@ -6,21 +6,6 @@ const axiosInstance = axios.create({
   withCredentials: true,
 });
 
-// // Add a response interceptor
-// axiosInstance.interceptors.response.use(
-//   function (response) {
-//     // Any status code that lie within the range of 2xx cause this function to trigger
-//     // Do something with response data
-//     return response;
-//   },
-//   function (error) {
-//     // Any status codes that falls outside the range of 2xx cause this function to trigger
-//     // Do something with response error
-//     console.log(error.response.data);
-//     return error.response;
-//   }
-// );
-
 // Login ,register,logout and authentication
 export const logoutUser = async () => {
   try {
