c4fe5506d9f25151f171c53e6b2c61ad0f96ce46
LalithadityaAnchula <lalithaditya903@gmail.com>
2022-07-08 14:57:12 +0530

    add not available messages

--- src/components/shared/AvailableCamps.jsx
+++ src/components/shared/AvailableCamps.jsx
@@ -6,14 +6,19 @@ export default function AvailableCamps({ role }) {
 
   return (
     <div className="columns is-multiline">
-      {camps.length !== 0 &&
+      {camps.length !== 0 ? (
         camps.map((camp) => {
           return (
             <div key={camp._id} className="column is-one-quarter">
               <CampCard camp={camp} role={role} />
             </div>
           );
-        })}
+        })
+      ) : (
+        <div className="section has-text-centered">
+          <h1 className="title is-2 has-text-grey">No available camps</h1>
+        </div>
+      )}
     </div>
   );
 }
